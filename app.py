5e63826276c5c9683d8017b25cb12eda8ec027a9
shabazkhan07759 <shabazkhan07759@gmail.com>
2026-03-26 13:07:01 +0530

    updated code

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
+import sqlite3
 import random
+import os
 
 app = Flask(__name__)
 
-orders = []
+# DB create
+def init_db():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS orders (
+            order_no INTEGER,
+            name TEXT,
+            item TEXT,
+            qty INTEGER,
+            total INTEGER
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+init_db()
 
 menu = {
     "Burger": 50,
-    "Pizza": 100,
+    "briyani": 100,
     "Sandwich": 40,
     "Tea": 10,
     "Coffee": 20
@@ -26,6 +46,20 @@ def order():
     price = menu[item] * qty
     order_no = random.randint(1000, 9999)
 
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO orders VALUES (?, ?, ?, ?, ?)",
+              (order_no, name, item, qty, price))
+
+    conn.commit()
+
+    # DEBUG (important)
+    print("Inserted:", order_no, name)
+    print("DB path:", os.path.abspath('database.db'))
+
+    conn.close()
+
     order_data = {
         "order_no": order_no,
         "name": name,
@@ -34,12 +68,28 @@ def order():
         "total": price
     }
 
-    orders.append(order_data)
-
     return render_template('result.html', order=order_data)
 
 @app.route('/admin')
 def admin():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM orders")
+    data = c.fetchall()
+
+    conn.close()
+
+    orders = []
+    for row in data:
+        orders.append({
+            "order_no": row[0],
+            "name": row[1],
+            "item": row[2],
+            "qty": row[3],
+            "total": row[4]
+        })
+
     return render_template('admin.html', orders=orders)
 
 if __name__ == '__main__':
